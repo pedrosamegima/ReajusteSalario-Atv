5138043a6ed30bd837f379f8c720071c910cc44e
Pedro Henrique <136722484+pedrosamegima@users.noreply.github.com>
2025-04-04 16:15:01 -0300

    Update main.cpp

--- ReajusteSalario/main.cpp
+++ ReajusteSalario/main.cpp
@@ -5,20 +5,20 @@ using namespace std;
 int main(){
     float salario, reajuste, sal_reajustado;
 
-    printf("Informe o salario do funcionario: ");
+    printf("Informe o salário do funcionario: ");
     scanf("%f", &salario);
 
     if(salario < 1500.00){
         reajuste = salario * 0.15;
         sal_reajustado = salario + reajuste;
-        printf("O salario reajustado com 15%% é de: %.2f\n", sal_reajustado);
+        printf("O salário reajustado com 15%% é de: %.2f\n", sal_reajustado);
     }else if(salario >=1500.00){
         reajuste = salario * 0.10;
         sal_reajustado = salario + reajuste;
-        printf("O salario reajustado com 10%% é de: %.2f\n", sal_reajustado);
+        printf("O salário reajustado com 10%% é de: %.2f\n", sal_reajustado);
     }else{
         reajuste = salario * 0.05;   
         sal_reajustado = salario + reajuste;
-        printf("O salario reajustado com 5%% é de: %.2f\n", sal_reajustado);
+        printf("O salário reajustado com 5%% é de: %.2f\n", sal_reajustado);
     }   
 }
